1fda8d1f673ce4cf24eb263d9295051d63306de5
jslyzx1991 <821203886@qq.com>
2019-01-24 10:18:40 +0800

    fix

--- src/pages/BxItem.vue
+++ src/pages/BxItem.vue
@@ -1,9 +1,30 @@
 <template>
-  <div class="bx-item">
+  <div class="bx-item mui-content-padded">
+    <div class="mui-input-group">
+      <div class="mui-input-row">
+        <label>报修条目</label>
+        <a href="#select" class="placeholder">报修条目</a>
+      </div>
+    </div>
     <input type="text" name="Project" v-model="Project">
     <input type="text" name="Image" v-model="Image">
     <input type="text" name="Remark" v-model="Remark">
     <button @click="replace">跳转</button>
+    <div id="picture" class="mui-popover mui-popover-action mui-popover-bottom">
+			<ul class="mui-table-view">
+				<li class="mui-table-view-cell">
+					<a href="#">拍照或录像</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a href="#">选取现有的</a>
+				</li>
+			</ul>
+			<ul class="mui-table-view">
+				<li class="mui-table-view-cell">
+					<a href="#picture"><b>取消</b></a>
+				</li>
+			</ul>
+		</div>
   </div>
 </template>
 <script>
@@ -17,9 +38,12 @@ export default {
       Remark: this.$route.query.Remark
     }
   },
+  created() {
+    this.$store.dispatch("getRepairSubjectList")
+  },
   methods: {
-  	replace: function(){
-  		
+    replace: function() {
+
       let data = {
         Project: this.Project,
         Remark: this.Remark,
@@ -29,7 +53,12 @@ export default {
       console.log(data)
       this.$store.dispatch("setBxItem", data)
       this.$router.replace('/life/bx/')
-  	}
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'repairSubjectList'
+    ])
   }
 }
 
