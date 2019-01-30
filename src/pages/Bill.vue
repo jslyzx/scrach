991decaf067c2a5a27bfddd372cb3b89c42cfabf
jslyzx <821203886@qq.com>
2019-01-30 22:27:36 +0800

    报修bug

--- src/pages/Bill.vue
+++ src/pages/Bill.vue
@@ -1,92 +1,92 @@
 <template>
-  <div>
-    <div v-if="userContractList.length === 1 || typeof this.$route.query.index !== 'undefined'">
-      <div class="tab-wrap">
-        <a href="javascript:;" :class="{active: !PayStatus}" @click="changeTab($event)">未付账单</a>
-        <a href="javascript:;" :class="{active: PayStatus}" @click="changeTab($event)">历史账单</a>
-      </div>
-      <div class="list">
-        <bill-list :status="PayStatus" :contract-id="ContractId"></bill-list>
-      </div>
+    <div>
+        <div v-if="userContractList.length === 1 || typeof this.$route.query.index !== 'undefined'">
+            <div class="tab-wrap">
+                <a href="javascript:;" :class="{active: !PayStatus}" @click="changeTab($event)">未付账单</a>
+                <a href="javascript:;" :class="{active: PayStatus}" @click="changeTab($event)">历史账单</a>
+            </div>
+            <div class="list">
+                <bill-list :status="PayStatus" :contract-id="ContractId"></bill-list>
+            </div>
+        </div>
+        <div v-else>
+            <contract-list :contract-list="userContractList" :redirect-url="url"></contract-list>
+        </div>
     </div>
-    <div v-else>
-      <contract-list :contract-list="userContractList" :redirect-url="url"></contract-list>
-    </div>
-  </div>
 </template>
 <script>
 import {
-  mapGetters
+    mapGetters
 } from 'vuex'
 import ContractList from '@/pages/ContractList'
 import BillList from '@/pages/BillList'
 export default {
-  data() {
-    return {
-      url: '/life/bill',
-      index: Number(this.$route.query.index || 0),
-      ContractId: 0,
-      PayStatus: 0
-    }
-  },
-  components: {
-    ContractList,
-    BillList
-  },
-  computed: {
-    ...mapGetters([
-      'userInfo',
-      'userContractList'
-    ])
-  },
-  created() {
-    this.$store.dispatch("getUserContractList", {
-      access_token: this.userInfo.token,
-      iskaimen: 0,
-      Status: 5
-    })
-  },
-  methods: {
-    changeTab(e) {
-      if (e.currentTarget.className !== 'active') {
-        this.PayStatus = (1 - this.PayStatus)
-      }
-    }
-  },
-  watch: {
-    '$route'(to, from) {
-      // 对路由变化作出响应...
-      this.index = Number(this.$route.query.index || 0)
-      this.ContractId = this.userContractList[this.index].Id
-    }
-  }
+    data() {
+            return {
+                url: '/life/bill',
+                index: Number(this.$route.query.index || 0),
+                ContractId: 0,
+                PayStatus: 0
+            }
+        },
+        components: {
+            ContractList,
+            BillList
+        },
+        computed: {
+            ...mapGetters([
+                'userInfo',
+                'userContractList'
+            ])
+        },
+        created() {
+            this.$store.dispatch("getUserContractList", {
+                access_token: this.userInfo.token,
+                iskaimen: 0,
+                Status: 5
+            })
+        },
+        methods: {
+            changeTab(e) {
+                if (e.currentTarget.className !== 'active') {
+                    this.PayStatus = (1 - this.PayStatus)
+                }
+            }
+        },
+        watch: {
+            '$route' (to, from) {
+                // 对路由变化作出响应...
+                debugger
+                if (to.path === '/life/bill') {
+                    this.index = Number(this.$route.query.index || 0)
+                    this.ContractId = this.userContractList[this.index].Id
+                }
+            }
+        }
 }
-
 </script>
 <style lang="scss" scoped>
 @import '../assets/css/function';
 .tab-wrap {
-  background: #fff;
-  height: px2rem(90px);
-  line-height: px2rem(90px);
-  a {
-    font-size: 16px;
-    color: #999;
-    padding: px2rem(25px) px2rem(23px);
-    &:nth-child(1) {
-      margin-left: px2rem(140px);
-      margin-right: px2rem(180px);
+    background: #fff;
+    height: px2rem(90px);
+    line-height: px2rem(90px);
+    a {
+        font-size: 16px;
+        color: #999;
+        padding: px2rem(25px) px2rem(23px);
+        &:nth-child(1) {
+            margin-left: px2rem(140px);
+            margin-right: px2rem(180px);
+        }
+        &:link,
+        &:hover {
+            text-decoration: none;
+        }
     }
-    &:link,
-    &:hover {
-      text-decoration: none;
+    .active {
+        color: #FF5252;
+        border-bottom: 1px solid #FF5252;
     }
-  }
-  .active {
-    color: #FF5252;
-    border-bottom: 1px solid #FF5252;
-  }
-
 }
-
 </style>
--- src/pages/BxItem.vue
+++ src/pages/BxItem.vue
@@ -1,133 +1,152 @@
 <template>
-  <div class="bx-item mui-content-padded">
-    <div class="mui-input-group">
-      <div class="mui-input-row">
-        <div class="mui-navigate-right">
-          <label>报修条目</label>
-          <a href="#select" class="placeholder" v-if="Project === ''">报修条目</a>
-          <a href="#select" v-else>{{Project}}</a>
+    <div class="bx-item mui-content-padded">
+        <div class="mui-input-group">
+            <div class="mui-input-row">
+                <div class="mui-navigate-right">
+                    <label>报修条目</label>
+                    <a href="#select" class="placeholder" v-if="Project === ''">报修条目</a>
+                    <a href="#select" v-else>{{Project}}</a>
+                </div>
+            </div>
         </div>
-      </div>
+        <el-upload action="/api/upload/image" list-type="picture-card" :on-preview="handlePictureCardPreview" :on-success="uploadSuccess" :on-remove="handleRemove">
+            <i class="el-icon-plus"></i>
+        </el-upload>
+        <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt="">
+        </el-dialog>
+        <div class="mui-input-row">
+            <textarea name="Remark" v-model="Remark" placeholder="故障描述"></textarea>
+        </div>
+        <button @click="replace">提交</button>
+        <div id="select" class="mui-popover mui-popover-action mui-popover-bottom">
+            <ul class="mui-table-view">
+                <li class="mui-table-view-cell" v-for="(item,index) in repairSubjectList">
+                    <a @click="choose(item.Name)">{{item.Name}}</a>
+                </li>
+            </ul>
+            <ul class="mui-table-view">
+                <li class="mui-table-view-cell">
+                    <a @click="hide"><b>取消</b></a>
+                </li>
+            </ul>
+        </div>
     </div>
-    <el-upload action="/api/upload/image" list-type="picture-card" :on-preview="handlePictureCardPreview" :on-success="uploadSuccess" :on-remove="handleRemove">
-      <i class="el-icon-plus"></i>
-    </el-upload>
-    <el-dialog :visible.sync="dialogVisible">
-      <img width="100%" :src="dialogImageUrl" alt="">
-	</el-dialog>
-      <div class="mui-input-row">
-        <textarea name="Remark" v-model="Remark" placeholder="故障描述"></textarea>
-      </div>
-      <button @click="replace">跳转</button>
-      <div id="select" class="mui-popover mui-popover-action mui-popover-bottom">
-        <ul class="mui-table-view">
-          <li class="mui-table-view-cell" v-for="(item,index) in repairSubjectList">
-            <a @click="choose(item.Name)">{{item.Name}}</a>
-          </li>
-        </ul>
-        <ul class="mui-table-view">
-          <li class="mui-table-view-cell">
-            <a @click="hide"><b>取消</b></a>
-          </li>
-        </ul>
-      </div>
-  </div>
 </template>
 <script>
-import { mapGetters } from 'vuex'
+import {
+    mapGetters
+} from 'vuex'
+import _ from 'lodash'
 
 export default {
-  data() {
-    return {
-      Project: this.$route.query.Project,
-      Image: this.$route.query.Image,
-      Remark: this.$route.query.Remark,
-      dialogImageUrl: '',
-      dialogVisible: false,
-      imgList: []
-    }
-  },
-  created() {
-    this.$store.dispatch("getRepairSubjectList")
-  },
-  methods: {
-    replace: function() {
-
-      let data = {
-        Project: this.Project,
-        Remark: this.Remark,
-        Image: this.imgList.join(''),
-        index: this.$route.query.index
-      }
-      console.log(data)
-      this.$store.dispatch("setBxItem", data)
-      this.$router.replace('/life/bx/')
-    },
-    hide: function() {
-      mui('#select').popover('hide')
-    },
-    choose: function(Name) {
-      this.Project = Name
-      mui('#select').popover('hide')
-    },
-    handleRemove(file, fileList) {
-      console.log(file, fileList);
-      var index = this.imgList.indexOf(file.response.numberData)
-      if(index > -1){
-      	this.imgList.splice(index, 1)
-      }
-    },
-    handlePictureCardPreview(file) {
-      this.dialogImageUrl = file.url;
-      this.dialogVisible = true;
-    },
-    uploadSuccess(response, file, fileList){
-    	this.imgList.push(response.numberData)
-    }
-  },
-  computed: {
-    ...mapGetters([
-      'repairSubjectList'
-    ])
-  },
-  mounted() {
-    mui.init({
-      swipeBack: true //启用右滑关闭功能
-    });
-  }
+    data() {
+            return {
+                Project: this.$route.query.Project,
+                Image: this.$route.query.Image,
+                Remark: this.$route.query.Remark,
+                dialogImageUrl: '',
+                dialogVisible: false,
+                imgList: []
+            }
+        },
+        created() {
+            this.$store.dispatch("getRepairSubjectList")
+            this.init()
+        },
+        watch: {
+            '$route' (to, from) {
+                if (to.path === '/life/bx/item') {
+                  this.init()
+                }
+            }
+        },
+        methods: {
+            init() {
+                this.Project = this.$route.query.Project
+                this.Image = this.$route.query.Image
+                this.Remark = this.$route.query.Remark
+                this.imgList = []
+                if (this.Image) {
+                    this.imgList = this.Image.split(';')
+                    this.imgList = _.compact(_.each(this.imgList, function(item) {
+                        item += ';'
+                    }))
+                }
+            },
+            replace: function() {
+                let data = {
+                    Project: this.Project,
+                    Remark: this.Remark,
+                    Image: this.imgList.join(''),
+                    index: this.$route.query.index
+                }
+                this.$store.dispatch("setBxItem", data)
+                this.$router.go(-1)
+            },
+            hide: function() {
+                mui('#select').popover('hide')
+            },
+            choose: function(Name) {
+                this.Project = Name
+                mui('#select').popover('hide')
+            },
+            handleRemove(file, fileList) {
+                console.log(file, fileList);
+                var index = this.imgList.indexOf(file.response.numberData)
+                if (index > -1) {
+                    this.imgList.splice(index, 1)
+                }
+            },
+            handlePictureCardPreview(file) {
+                this.dialogImageUrl = file.url;
+                this.dialogVisible = true;
+            },
+            uploadSuccess(response, file, fileList) {
+                this.imgList.push(response.numberData)
+            }
+        },
+        computed: {
+            ...mapGetters([
+                'repairSubjectList'
+            ])
+        },
+        mounted() {
+            mui.init({
+                swipeBack: true //启用右滑关闭功能
+            });
+        }
 }
-
 </script>
 <style lang="scss">
 @import '../assets/css/function';
 .bx-item {
-  .mui-input-group {
-    margin-bottom: 20px;
-  }
-  .mui-navigate-right {
-    a {
-      float: right;
-      position: relative;
-      right: 30px;
-      top: 10px;
+    .mui-input-group {
+        margin-bottom: 20px;
     }
-  }
-  textarea {
-    margin-top: 20px;
-    height: px2rem(200px);
-  }
-  .el-upload--picture-card {
-    width: px2rem(200px);
-    height: px2rem(200px);
-    line-height: px2rem(200px);
-  }
-  .el-upload--picture-card i {
-    line-height: px2rem(200px);
-  }
-  .el-upload-list--picture-card .el-upload-list__item {
-    width: px2rem(200px);
-    height: px2rem(200px);
-  }
+    .mui-navigate-right {
+        a {
+            float: right;
+            position: relative;
+            right: 30px;
+            top: 10px;
+        }
+    }
+    textarea {
+        margin-top: 20px;
+        height: px2rem(200px);
+    }
+    .el-upload--picture-card {
+        width: px2rem(200px);
+        height: px2rem(200px);
+        line-height: px2rem(200px);
+    }
+    .el-upload--picture-card i {
+        line-height: px2rem(200px);
+    }
+    .el-upload-list--picture-card .el-upload-list__item {
+        width: px2rem(200px);
+        height: px2rem(200px);
+    }
 }
-
 </style>
